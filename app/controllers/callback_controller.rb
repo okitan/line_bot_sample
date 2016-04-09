5403076f01a18eff1b81a88b7378b4e325ccd7c7
okitan <okitakunio@gmail.com>
2016-04-09 18:32:47 +0900

    responding

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -1,9 +1,17 @@
 class CallbackController < ApplicationController
   protect_from_forgery except: :create
 
+  OP_ADDED_TO_FRIEND = 4
+  OP_BLOCKED         = 8
+
   def create
-    callbacks.each do |msg|
+    contents.each do |msg|
       p msg
+      if msg.has_key?("opType")
+        respond_to_friend_request(msg)
+      else
+        respond_to_talk(msg)
+      end
     end
 
     respond_to do |format|
@@ -11,8 +19,30 @@ class CallbackController < ApplicationController
     end
   end
 
+  protected
+  def respond_to_friend_request(msg)
+    case msg["opType"]
+    when OP_ADDED_TO_FRIEND
+      # TODO: enabled
+      puts "TODO: started taking with channel #{msg["params"]}"
+    when OP_BLOCKED
+      # TODO: disabled
+      puts "TODO: stop taking to channel #{msg["params"]}"
+    else
+      puts "unknown opType #{msg["opType"]}"
+    end
+  end
+
+  def respond_to_talk(msg)
+    puts "TODO: responding to talk"
+  end
+
   private
   def callbacks
     params.require(:callback).require(:result)
+  end
+
+  def contents
+    callbacks.map {|e| e["content"] }
   end
 end
